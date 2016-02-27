3d68ad08c11efdb1d8142f9ca8a9e82d776d284f
daose <bychen@uwaterloo.ca>
2016-02-27 13:53:47 -0500

    node firebase added on test.js

---
+++ test.js
@@ -0,0 +1,39 @@
+var Firebase = require('firebase');
+
+var rootRef = new Firebase('https://fiery-heat-3854.firebaseio.com/')
+var step;
+var myJson;
+rootRef = new Firebase('https://fiery-heat-3854.firebaseio.com/');
+
+var stepRef = rootRef.child('step');
+var recipeRef = rootRef.child('recipe');
+
+        rootRef.on('value', function(rootSnapshot){
+          rootRef.child('step').on('value', function(stepSnapshot){
+            step = stepSnapshot.val();
+          });
+          rootRef.child('recipe').on('value', function(recipeSnapshot){
+            myJson = recipeSnapshot.val();
+            console.log("Step " + (step + 1) + ", " + myJson[step]);
+          });
+        });
+
+        //Alexa what was step __ again?
+        setBtn.addEventListener('click', function(){
+            stepRef.set(+(document.getElementById('stepNumber').value) - 1);
+        })
+
+        //Alexa, next step please
+        nextBtn.addEventListener('click', function(){
+            stepRef.set((step + 1));
+        });
+
+        //Alexa, could you repeat the step again?
+        currBtn.addEventListener('click', function(){
+          console.log("Step " + (step + 1) + ", " + myJson[step]);
+        });
+
+        //Alexa, what was the last step?
+        prevBtn.addEventListener('click', function(){
+            stepRef.set((step - 1));
+        });

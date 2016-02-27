45396714aca8b93551788b85adc0af041595f5af
Danyal Raza <smdraza@uwaterloo.ca>
2016-02-27 17:46:27 -0500

    new things

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ var http = require('http');
 var Firebase = require("firebase");
 var express = require("express");
 
+
 var app = express();
 
 var apiKey="1a2pNH4Ed5Yt15zsqR28Q2MUXJt4gG7B";
 
-var findRecipeByName = function(recipeName, callback) {
+
+app.get('/search/:searchkey', function(request, response) {
+  var recipeName = request.params.searchkey;
   var host = "api.bigoven.com"
   var path = "/recipes?pg=1&rpp=25&title_kw="
             + recipeName
             + "&api_key=" + apiKey + "&sort=quality";
-
+  console.log(recipeName);
   var post_options = {
       hostname: host,
       path: path,
@@ -24,24 +27,15 @@ var findRecipeByName = function(recipeName, callback) {
       var output = "";
       var recipesObject = {};
       var recipesArray = [];
-      var topFiveRecipes = [];
-
       res.on("data", function (chunk) {
           output += chunk
       })
-      res.on("error", console.log);
+      res.on("error", console.log)
       res.on("end", function () {
-          return output;
+          recipesObject = JSON.parse(output);
+          response.json(recipesObject);
       })
   }).end();
-}
-
-app.get('/search/:searchkey', function(request, response, next) {
-  var searchkey = request.params.searchkey;
-  findRecipeByName(searchkey, function(error, recipes) {
-    if (error) return error;
-    return response.render(recipes);
-  });
 });
 
 app.listen(port=8000);

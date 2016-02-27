5699d7b9fd9aab04bf3a2811fdf44d7253936c39
danyalraza <danyalraza@users.noreply.github.com>
2016-02-27 08:17:48 -0500

    Made changes

--- sample/parser.js
+++ sample/parser.js
@@ -1,6 +1,178 @@
+var myObj = {
+  "RecipeID": 179919,
+  "Title": "Almond Crusted Chicken Breast",
+  "Description": "This is a relative of my original Viennese Schnitzel. It's incredibly delicous, quick and most of all healthy.",
+  "Cuisine": "American",
+  "Category": "Main Dish",
+  "Subcategory": "Poultry - Chicken",
+  "Microcategory": null,
+  "PrimaryIngredient": "Chicken",
+  "StarRating": 5,
+  "WebURL": "http://www.bigoven.com/recipe/almond-crusted-chicken-breast/179919",
+  "ImageURL": "http://redirect.bigoven.com/pics/rs/640/almond-crusted-chicken-breast.jpg",
+  "ReviewCount": 1,
+  "MedalCount": 0,
+  "FavoriteCount": 48,
+  "Poster": {
+    "UserID": 753655,
+    "UserName": "jsvendblad",
+    "ImageURL48": "http://images.bigoven.com/image/upload/t_recipe-48,d_avatar-default.png/avatar/200410021811753655.jpg",
+    "PhotoUrl": "http://photos.bigoven.com/avatar/photo/200410021811753655.jpg",
+    "IsPremium": true,
+    "IsKitchenHelper": false,
+    "PremiumExpiryDate": "/Date(4102444800000)/",
+    "MemberSince": "/Date(1265284260000)/",
+    "IsUsingRecurly": false,
+    "FirstName": null,
+    "LastName": null
+  },
+  "Ingredients": [
+    {
+      "IngredientID": 1852810,
+      "DisplayIndex": 0,
+      "IsHeading": false,
+      "Name": "Chicken breasts",
+      "HTMLName": "<a href=\"http://www.bigoven.com/glossary/chicken%20breast\" class=\"glosslink\">Chicken breast</a>s",
+      "Quantity": 4,
+      "DisplayQuantity": "4",
+      "Unit": "6 oz",
+      "MetricQuantity": 4,
+      "MetricDisplayQuantity": "4",
+      "MetricUnit": "6 oz",
+      "PreparationNotes": "butterflied",
+      "IngredientInfo": null,
+      "IsLinked": false
+    },
+    {
+      "IngredientID": 1852811,
+      "DisplayIndex": 1,
+      "IsHeading": false,
+      "Name": "Coconut oil",
+      "HTMLName": "<a href=\"http://www.bigoven.com/glossary/coconut\" class=\"glosslink\">Coconut</a> oil",
+      "Quantity": 2,
+      "DisplayQuantity": "2",
+      "Unit": "tbsp",
+      "MetricQuantity": 30,
+      "MetricDisplayQuantity": "30",
+      "MetricUnit": "ml",
+      "PreparationNotes": null,
+      "IngredientInfo": {
+        "Name": "Coconut",
+        "Department": "Produce",
+        "MasterIngredientID": 321,
+        "UsuallyOnHand": false
+      },
+      "IsLinked": true
+    },
+    {
+      "IngredientID": 1852812,
+      "DisplayIndex": 2,
+      "IsHeading": false,
+      "Name": "Almond Flour",
+      "HTMLName": "<a href=\"http://www.bigoven.com/glossary/almond\" class=\"glosslink\">Almond</a> Flour",
+      "Quantity": 1,
+      "DisplayQuantity": "1",
+      "Unit": "cup",
+      "MetricQuantity": 237,
+      "MetricDisplayQuantity": "237",
+      "MetricUnit": "ml",
+      "PreparationNotes": "blanched ",
+      "IngredientInfo": null,
+      "IsLinked": true
+    },
+    {
+      "IngredientID": 1852813,
+      "DisplayIndex": 3,
+      "IsHeading": false,
+      "Name": "Sea salt",
+      "HTMLName": "Sea <a href=\"http://www.bigoven.com/glossary/salt\" class=\"glosslink\">salt</a>",
+      "Quantity": 3,
+      "DisplayQuantity": "3",
+      "Unit": "pinches ",
+      "MetricQuantity": 3,
+      "MetricDisplayQuantity": "3",
+      "MetricUnit": "pinches ",
+      "PreparationNotes": "to taste",
+      "IngredientInfo": {
+        "Name": "Salt",
+        "Department": "Spices",
+        "MasterIngredientID": 6,
+        "UsuallyOnHand": true
+      },
+      "IsLinked": true
+    }
+  ],
+  "Instructions": "Always use organic if you can.  Chicken breast should be butterflied and slighlty tenderized on one side.  Sea salt lightly on both sides and coat with blanched almond flour.  Pad chicken firmly and carefully shake off excess flour.  Place into non-stick pan with hot coconut oil.  After about 30 seconds, reduce heat to medium and cook for about 2 minutes on each side. \r\n\r\nIt's a good idea to pre-heat oven to 200 just to keep the first couple of chicken breasts warm.  It's best to use a cookie sheet lined with foil so to make sure not to place them on top of each other.  \r\n\r\nThis is an amazing dish, serve with the veggie of your choice or a delicous salad. \r\n\r\n",
+  "YieldNumber": 2,
+  "YieldUnit": "Servings",
+  "TotalMinutes": 10,
+  "ActiveMinutes": 0,
+  "NutritionInfo": {
+    "SingularYieldUnit": "Paid API plan required for nutrition.",
+    "TotalCalories": 0,
+    "TotalFat": 0,
+    "CaloriesFromFat": 0,
+    "TotalFatPct": 0,
+    "SatFat": 0,
+    "SatFatPct": 0,
+    "MonoFat": 0,
+    "PolyFat": 0,
+    "TransFat": 0,
+    "Cholesterol": 0,
+    "CholesterolPct": 0,
+    "Sodium": 0,
+    "SodiumPct": 0,
+    "Potassium": 0,
+    "PotassiumPct": 0,
+    "TotalCarbs": 0,
+    "TotalCarbsPct": 0,
+    "DietaryFiber": 0,
+    "DietaryFiberPct": 0,
+    "Sugar": 0,
+    "Protein": 0,
+    "ProteinPct": 0
+  },
+  "IsPrivate": null,
+  "CreationDate": "/Date(1268248319000)/",
+  "LastModified": "/Date(1413376299960)/",
+  "IsBookmark": false,
+  "BookmarkURL": null,
+  "BookmarkSiteLogo": "",
+  "BookmarkImageURL": null,
+  "IsRecipeScan": null,
+  "MenuCount": 4,
+  "NotesCount": 0,
+  "AdTags": "simplypotatoes15",
+  "IngredientsTextBlock": null,
+  "AllCategoriesText": "low sugar|diabetic|atkins-friendly|kid friendly|winter|valentines day|thanksgiving|summer|spring|new year|fourth of july|fall|easter|christmas|quick|fry|main dish|chicken|american|collxthanksgiving|",
+  "IsSponsored": false,
+  "VariantOfRecipeID": null,
+  "Collection": "",
+  "CollectionID": null,
+  "AdminBoost": null,
+  "VerifiedDateTime": null,
+  "MaxImageSquare": 640,
+  "ImageSquares": [
+    640,
+    512,
+    480,
+    320,
+    256,
+    200,
+    128,
+    120,
+    64,
+    48,
+    36
+  ],
+  "HeroPhotoUrl": "http://photos.bigoven.com/recipe/hero/almond-crusted-chicken-breast.jpg",
+  "VerifiedByClass": null
+};
+
 var newObj = {};
 newObj.Title = myObj.Title;
 newObj.Ingredients = myObj.Ingredients
+console.log(newObj);
 newObj.Instructions = myObj.Instructions;
 var newstring = newObj.Instructions.replace(/[&\/\\#,+()$~%'":*?<>\r{}\n]/g, '').replace("..", "").trim();
 var array = newstring.split('. ')
@@ -13,3 +185,4 @@ for (var i = 0; i < array.length; i ++) {
 }
 newObj.Instructions = replaceObj;
 newObj.CurrentStep = 0;
+console.log(newObj);
